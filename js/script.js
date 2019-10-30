fb6745baf252c4e577745a4c8190033b837038d6
Marcos Allysson <marcos.silva3@estudante.ifb.edu.br>
2019-10-29 22:00:25 -0300

    changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// criação do array:
+// criação do array: 
 (function(){
     let tiles = []; // armanezar as peças no array...
     let answer = []; // armanezar resposta de verificação do jogo
@@ -26,6 +26,88 @@
 
         render(); // chamar função pra espalhar as peças no tabuleiro
     }
+
+
+    // desenhar o array 
+    function render(){ 
+        for(let i in tiles){ 
+            let tile = tiles[i];  // pros índices de tiles receber o valor do array, 
+            
+            if(tile){ // verificar se há valor no índice. Porque tem uma peça em branco...
+                //ter como referência a margem esquerda + 5px de distância. Vai fazer distribuição das peças em relação a margem esquerda
+                tile.style.left = (i % 3) * 100 + 5 + "px"; 
+                if(i < 3){
+                    tile.style.top = "5px"; // margem de cima
+                } 
+                else if(i < 6){
+                    tile.style.top = "105px"; // segunda linha
+                } else {
+                    tile.style.top = "205px"; // terceira linha
+                }
+            }  
+        }
+    }
+
+
+    //embaralhar as peças. Além de verificar se, a cada índice do array, já existe outro de mesmo valor, 
+    //se não, ele é inserido no novo array. Vai fazer isso até retornar todos os elementos do outro array, de forma aleatória
+    function randomSort(oldArray){ // array antigo
+        let newArray;
+        //let erros = 0; // pra verificar quantos arrays ele gera que não poderiam ser resolvidos
+        //newArray = [];
+
+        do{
+            newArray = [];
+            while(newArray.length < oldArray.length){ // enquanto o novo array for menor do que a qnt do array recebido:
+                // retorna valor inteiro arrendondado pra baixo. Random gera número aleatório * o comprimento do array antigo
+                let i = Math.floor(Math.random()*oldArray.length); 
+                
+                if(newArray.indexOf(oldArray[i]) < 0){ // verificar se o valor do índice no array antigo já existe no novo array... 
+                    newArray.push(oldArray[i]); //Se não existir, retorna -1, como é menor que 0, é inserido no novo array
+                }
+            } 
+            //erros ++; // acrescentando +1 na variável erros de arrays sem solução
+            //console.log(erros); printando erros
+        } while(!validGame(newArray)); // vai ficar chamando a função até que ela retorne um jogo que pode ser resolvido
+
+        return newArray; // depois que tiver todos os elementos, o novo array é retornado.
+    }
+
+
+    // função pra validar se o jogo pode ser resolvido ou não
+    function validGame(array){
+        let inversions = 0;
+        let len = array.length;
+
+        for(let i = 0; i < len -1; i+=1){
+            for(let m = i+1; m < len; m +=1){
+                if(array[i] && array[m] && array[i].dataset.value < array[m].dataset.value){
+                    inversions +=1;
+                }
+            }
+        }
+
+        return inversions % 2 === 0; // se false for retornado, o jogo não tem solução
+    }
+
+
+    // função que inicia o jogo
+    function startGame(){
+        setTimeout(function(){
+            alert('Você não conseguiu resolver...');
+            gameOver();
+        }, 10000);
+
+        // embaralhar as peças. A função recebe o array em ordem, e retorna o array embaralhado pra variável
+        tiles = randomSort(tiles);  
+        
+        this.style.opacity = "0";  // this serve pra que, quem chamar, sofra a ação do jogo
+        this.style.zIndex = "-1"; // pra tirar a div da tela inicial e possibilidade acesso às peças
+        this.removeEventListener("click", startGame, false);  // remover o evento de click, já que o jogo já foi inicializado
+
+        render(); // é chamada pra reformular a exibição das formas no tabuleiro;
+    }
+
 
 
     // função pra movimentar a peça
@@ -96,80 +178,7 @@
             overScreen.addEventListener("click", startGame, false);
         }, 500);
     }
-
-
-    // desenhar o array 
-    function render(){ 
-        for(let i in tiles){ 
-            let tile = tiles[i];  // pros índices de tiles receber o valor do array, 
-            
-            if(tile){ // verificar se há valor no índice. Porque tem uma peça em branco...
-                //ter como referência a margem esquerda + 5px de distância. Vai fazer distribuição das peças em relação a margem esquerda
-                tile.style.left = (i % 3) * 100 + 5 + "px"; 
-                if(i < 3){
-                    tile.style.top = "5px"; // margem de cima
-                } 
-                else if(i < 6){
-                    tile.style.top = "105px"; // segunda linha
-                } else {
-                    tile.style.top = "205px"; // terceira linha
-                }
-            }  
-        }
-    }
-
-    //embaralhar as peças. Além de verificar se, a cada índice do array, já existe outro de mesmo valor, 
-    //se não, ele é inserido no novo array. Vai fazer isso até retornar todos os elementos do outro array, de forma aleatória
-    function randomSort(oldArray){ // array antigo
-        let newArray;
-        let erros = 0; // pra verificar quantos arrays ele gera que não poderiam ser resolvidos
-        //newArray = [];
-
-        do{
-            newArray = [];
-            while(newArray.length < oldArray.length){ // enquanto o novo array for menor do que a qnt do array recebido:
-                // retorna valor inteiro arrendondado pra baixo. Random gera número aleatório * o comprimento do array antigo
-                let i = Math.floor(Math.random()*oldArray.length); 
-                
-                if(newArray.indexOf(oldArray[i]) < 0){ // verificar se o valor do índice no array antigo já existe no novo array... 
-                    newArray.push(oldArray[i]); //Se não existir, retorna -1, como é menor que 0, é inserido no novo array
-                }
-            } 
-            //erros ++; // acrescentando +1 na variável erros de arrays sem solução
-            //console.log(erros); printando erros
-        } while(!validGame(newArray)); // vai ficar chamando a função até que ela retorne um jogo que pode ser resolvido
-
-        return newArray; // depois que tiver todos os elementos, o novo array é retornado.
-    }
     
-    // função que inicia o jogo
-    function startGame(){
-        // embaralhar as peças. A função recebe o array em ordem, e retorna o array embaralhado pra variável
-        tiles = randomSort(tiles);  
-        
-        this.style.opacity = "0";  // this serve pra que, quem chamar, sofra a ação do jogo
-        this.style.zIndex = "-1"; // pra tirar a div da tela inicial e possibilidade acesso às peças
-        this.removeEventListener("click", startGame, false);  // remover o evento de click, já que o jogo já foi inicializado
-
-        render(); // é chamada pra reformular a exibição das formas no tabuleiro;
-    }
-
-    
-    // função pra validar se o jogo pode ser resolvido ou não
-    function validGame(array){
-        let inversions = 0;
-        let len = array.length;
-
-        for(let i = 0; i < len -1; i+=1){
-            for(let m = i+1; m < len; m +=1){
-                if(array[i] && array[m] && array[i].dataset.value < array[m].dataset.value){
-                    inversions +=1;
-                }
-            }
-        }
-
-        return inversions % 2 === 0; // se false for retornado, o jogo não tem solução
-    }
 
     init();
 }());
