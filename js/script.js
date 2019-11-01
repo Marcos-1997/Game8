cf935495533340fa821be2ba10d8059a35d952f5
Marcos Allysson <marcos.silva3@estudante.ifb.edu.br>
2019-11-01 20:20:00 -0300

    final

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
-// criação do array: 
+// criação do array:  
 let pecas = []; // armanezar as peças no array...
 let arrayEmOrdem = []; // armanezar resposta de verificação do jogo
 let telaInicial = document.getElementById('telaInicial'); // pegando tela de início
-//Evento que vai ser chamado quando for clicado na tela inicial. Recebendo false, pra que nenhuma outra função possa chama-la
-telaInicial.addEventListener("click", iniciarJogo, false);
-
+telaInicial.addEventListener("click", iniciarJogo); //Evento que vai ser chamado quando for clicado na tela inicial. 
 let telaFinal = document.getElementById('telaFinal'); // pegando tela final de jogo
 
 
 // varrer o html pra pegar as referências e salvar no array
-function varrerHtml() { 
+function varrerHtml() {
     for (let i = 1; i < 9; i += 1) { // varrer o array
         let peca = document.getElementById('n' + i); // cada referência as peças do jogo
 
@@ -17,7 +15,7 @@ function varrerHtml() {
         peca.style.background = "url('images/n" + i + ".png')";
 
         // disparar função quando uma peça for clicada. Vai chamar função pra que a peça possa ser movimentada.
-        peca.addEventListener("click", moverPeca, false);
+        peca.addEventListener("click", moverPeca);
 
         pecas.push(peca); // o array recebe uma peça
     }
@@ -28,6 +26,9 @@ function varrerHtml() {
     desenharArray(); // chamar função pra espalhar as peças no tabuleiro
 }
 
+// chamando função pra pegar referências do html...
+varrerHtml();
+
 
 // desenhar o array 
 function desenharArray() {
@@ -35,11 +36,11 @@ function desenharArray() {
         let peca = pecas[i];  // pros índices de pecas receber o valor do array, 
 
         if (peca) { // verificar se há valor no índice. Porque tem uma peça em branco...
-            //ter como referência a margem esquerda + 5px de distância. Vai fazer distribuição das peças em relação a margem esquerda
+            //ter como referência a margem esquerda + 5px de distância. Fazendo a distribuição das peças.
             peca.style.left = (i % 3) * 100 + 5 + "px";
             if (i < 3) { peca.style.top = "5px"; } // margem de cima
-            else if (i < 6) { peca.style.top = "105px";  } // segunda linha
-            else { peca.style.top = "205px";  } // terceira linha
+            else if (i < 6) { peca.style.top = "105px"; } // segunda linha
+            else { peca.style.top = "205px"; } // terceira linha
         }
     }
 }
@@ -47,39 +48,44 @@ function desenharArray() {
 
 //embaralhar as peças. Além de verificar se, a cada índice do array, já existe outro de mesmo valor, 
 //se não, ele é inserido no novo array. Vai fazer isso até retornar todos os elementos do outro array, de forma aleatória
-function embaralharPecas(oldArray) { // array antigo
-    let newArray;
-    
-    do {
-        newArray = [];
-        while (newArray.length < oldArray.length) { // enquanto o novo array for menor do que a qnt do array recebido:
-            // retorna valor inteiro arrendondado pra baixo. Random gera número aleatório * o comprimento do array antigo
-            let i = Math.floor(Math.random() * oldArray.length);
+function embaralharPecas(antigoArray) { // array antigo
+    let novoArray;
+    //let contadorErro = 0; // verificar quantos arrays inválidos ele gera
 
-            if (newArray.indexOf(oldArray[i]) < 0) { // verificar se o valor do índice no array antigo já existe no novo array... 
-                newArray.push(oldArray[i]); //Se não existir, retorna -1, como é menor que 0, é inserido no novo array
+    do {
+        novoArray = [];
+        while (novoArray.length < antigoArray.length) { // enquanto o novo array for menor do que a qnt do array antigo:
+            // retorna valor inteiro arrendondado pra baixo. Random gera número aleatório * o tamanho do array antigo
+            let i = Math.floor(Math.random() * antigoArray.length);
+
+            if (novoArray.indexOf(antigoArray[i]) < 0) { // verificar se o valor do índice no array antigo já existe no novo array... 
+                novoArray.push(antigoArray[i]); //Se não existir, retorna -1, como é menor que 0, é inserido no novo array
             }
         }
-    } while (!validarJogo(newArray)); // vai ficar chamando a função até que ela retorne um jogo que pode ser resolvido
+        //contadorErro +=1; // 
+        //console.log(contadorErro); // printando array inválido
+    } while (!validarJogo(novoArray)); // vai ficar chamando a função até que ela retorne um jogo que pode ser resolvido
 
-    return newArray; // depois que tiver todos os elementos, o novo array é retornado.
+    return novoArray; // depois que tiver todos os elementos, o novo array é retornado.
 }
 
 
-// função pra validar se o jogo pode ser resolvido ou não
+//função pra validar se o jogo pode ser resolvido ou não.
 function validarJogo(array) {
+    // inversões -> se o elemento é maior que o próximo elemento, conta como uma inversão. 
     let inversoes = 0;
-    let len = array.length;
+    let tamanho = array.length;
 
-    for (let i = 0; i < len - 1; i += 1) {
-        for (let m = i + 1; m < len; m += 1) {
-            if (array[i] && array[m] && array[i].dataset.value < array[m].dataset.value) {
-                inversoes += 1;
-            }
+    for(let i = 0; i <= tamanho; i += 1) { //varrer array [i]
+        // pra cada elemento [i], onde recebe ele mesmo mais 1, o proximo elemento
+        for (let m = i + 1; m < tamanho; m += 1) { 
+            // se o array tiver um elemento [i], [m] e se tiver no elemento declarado no html, for maior do que o array no índice m,
+            // inversoes recebe +1
+            if (array[i] && array[m] && array[i].dataset.value > array[m].dataset.value) { inversoes += 1; }
         }
     }
 
-    return inversoes % 2 === 0; // se false for retornado, o jogo não tem solução
+    return inversoes % 2 === 0; // se for par, jogo válido, se não, o jogo não tem solução.
 }
 
 
@@ -88,14 +94,14 @@ function iniciarJogo() {
     setTimeout(function () {
         alert('Você não conseguiu resolver...');
         fimDeJogo();
-    }, 50000);
+    }, 50000); // 50 segundos pra solucionar o jogo
 
     // embaralhar as peças. A função recebe o array em ordem, e retorna o array embaralhado pra variável
     pecas = embaralharPecas(pecas);
 
     this.style.opacity = "0";  // this serve pra que, quem chamar, sofra a ação do jogo
     this.style.zIndex = "-1"; // pra tirar a div da tela inicial e possibilidade acesso às peças
-    this.removeEventListener("click", iniciarJogo, false);  // remover o evento de click, já que o jogo já foi inicializado
+    this.removeEventListener("click", iniciarJogo);  // remover o evento de click, já que o jogo já foi inicializado
 
     desenharArray(); // é chamada pra reformular a exibição das formas no tabuleiro;
 }
@@ -165,9 +171,6 @@ function fimDeJogo() {
     telaFinal.style.zIndex = "1";
 
     setTimeout(function () { // como o modal tem transição de 0.5s, o função espera meio segundo também
-        telaFinal.addEventListener("click", iniciarJogo, false);
+        telaFinal.addEventListener("click", iniciarJogo);
     }, 500);
 }
-
-
-varrerHtml(); // chamando função pra pegar referências do html...
